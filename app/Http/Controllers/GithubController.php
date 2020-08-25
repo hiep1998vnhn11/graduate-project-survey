ea3373dd7bb5bebea45dadfa464e0bfed09d71ae
Sotatek-HiepTran <hiep.tran@sotatek.com>
2020-08-25 14:11:15 +0700

    login with github completed

---
+++ app/Http/Controllers/GithubController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class GithubController extends Controller
+{
+    const SERVICE = "github";
+
+    public function redirectLogin()
+    {
+        return Socialite::driver(static::SERVICE)->redirect();
+    }
+
+    public function handleCallback() {
+        try {
+            $user = Socialite::driver(static::SERVICE)->user();
+            // check if user registered or not in the system
+            $userDB = User::where('email', $user->getEmail())->first();
+
+            // if not registered yet, create a new user record
+            if (!$userDB) {
+                $userDB = new User();
+                if(!$user->getName()) $userDB->name = $user->getNickname();
+                else $userDB->name = $user->getName();
+                $userDB->email = $user->getEmail();
+                // more info if you have any
+                $userDB->save();
+                // if db error
+                // ok, it saved. login now
+            }
+            // login to the system now
+            Auth::loginUsingId($userDB->id);
+
+            // redirect back to homepage with success
+            return redirect()
+                    ->to('/home')
+                    ->with('success', 'Login successfully!');
+
+        } catch (\Exception $e) {
+            return redirect()
+                ->to('/login')
+                ->with('error', 'Login via Github failed. Please try again.');
+        }
+    }
+}
